5da31ca2855a9d8fbe49ef59b4f37428c12392d1
mubarak giwa <mubarakgiwa72@gmail.com>
2026-08-29 16:41:09 +0100

    vercel update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/mubarak-portfolio/",
+    base: '/', // Ensures assets are fetched relative to the root path
   build: {
   chunkSizeWarningLimit: 1000,
 }
