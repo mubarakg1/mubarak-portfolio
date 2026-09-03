c4d512ea4613ce48e2497868f51aa3c0b704ae85
mubarak giwa <mubarakgiwa72@gmail.com>
2026-09-03 07:52:55 +0100

    vite config adjustment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // Automatically switches base path between GitHub Pages and Vercel
+  // Sets the base folder for GitHub Actions, defaults to root for Vercel
   base: process.env.GITHUB_ACTIONS ? "/mubarak-portfolio/" : "/",
   build: {
     chunkSizeWarningLimit: 1000,
