c87a39e21428ba6eccfb5110acdeaa405522aed1
mubarak giwa <mubarakgiwa72@gmail.com>
2026-09-03 08:10:57 +0100

    gitpage  updates

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/mubarak-portfolio/",
-    
+  // Setting base to './' forces relative paths so it works everywhere
+  base: './', 
   build: {
-  chunkSizeWarningLimit: 1000,
-}
+    chunkSizeWarningLimit: 1000,
+  }
 });
